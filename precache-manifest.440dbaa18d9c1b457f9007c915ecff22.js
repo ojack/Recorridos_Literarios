77ea88743723756a2f7b102e0deb988205b4fd8b
OLIJAC <ojackson@gmail.com>
2018-12-10 14:47:27 -0500

    Updates

---
+++ precache-manifest.440dbaa18d9c1b457f9007c915ecff22.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c159d0d05473040b53ec79df8797d32",
+    "url": "/recorridos-literarios/static/media/ionicons.2c159d0d.woff"
+  },
+  {
+    "revision": "a9368ca4c588c07c91ec",
+    "url": "/recorridos-literarios/static/css/main.0a6f68ec.chunk.css"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/recorridos-literarios/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "06ac2d2b3b17da9ded6e",
+    "url": "/recorridos-literarios/static/js/1.06ac2d2b.chunk.js"
+  },
+  {
+    "revision": "daeaedcabe4a0df87774",
+    "url": "/recorridos-literarios/static/js/runtime~main.daeaedca.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/recorridos-literarios/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/recorridos-literarios/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/recorridos-literarios/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/recorridos-literarios/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/recorridos-literarios/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "8d9ab84bfe87a3f77112a6698cf639fb",
+    "url": "/recorridos-literarios/static/media/fa-regular-400.8d9ab84b.woff2"
+  },
+  {
+    "revision": "dad90637f797356bbc70d2664832e0b6",
+    "url": "/recorridos-literarios/static/media/fa-regular-400.dad90637.woff"
+  },
+  {
+    "revision": "e6c93cb47e716b579264a5fdfbf7e84d",
+    "url": "/recorridos-literarios/static/media/fa-regular-400.e6c93cb4.eot"
+  },
+  {
+    "revision": "8d220c793e2612bd131ed8522c54669f",
+    "url": "/recorridos-literarios/static/media/fa-regular-400.8d220c79.ttf"
+  },
+  {
+    "revision": "a4d31128b633bc0b1cc1f18a34fb3851",
+    "url": "/recorridos-literarios/static/media/Material-Design-Iconic-Font.a4d31128.woff2"
+  },
+  {
+    "revision": "6814d0e8136d34e313623eb7129d538e",
+    "url": "/recorridos-literarios/static/media/fa-brands-400.6814d0e8.woff2"
+  },
+  {
+    "revision": "b75b4bfe0d58faeced5006c785eaae23",
+    "url": "/recorridos-literarios/static/media/fa-solid-900.b75b4bfe.woff2"
+  },
+  {
+    "revision": "a9368ca4c588c07c91ec",
+    "url": "/recorridos-literarios/static/js/main.a9368ca4.chunk.js"
+  },
+  {
+    "revision": "d2a55d331bdd1a7ea97a8a1fbb3c569c",
+    "url": "/recorridos-literarios/static/media/Material-Design-Iconic-Font.d2a55d33.woff"
+  },
+  {
+    "revision": "da408238128b876cbda6424391f1566f",
+    "url": "/recorridos-literarios/static/media/fa-brands-400.da408238.woff"
+  },
+  {
+    "revision": "2d0415fa29ea596b7a02c78eddeede20",
+    "url": "/recorridos-literarios/static/media/fa-solid-900.2d0415fa.woff"
+  },
+  {
+    "revision": "e8019d507e8cb51d169ab4f94a0cda12",
+    "url": "/recorridos-literarios/static/media/fa-brands-400.e8019d50.eot"
+  },
+  {
+    "revision": "fdf44bc43e8fa2358bbb7d9165d78455",
+    "url": "/recorridos-literarios/static/media/fa-brands-400.fdf44bc4.ttf"
+  },
+  {
+    "revision": "19e65b89cee273a249fba4c09b951b74",
+    "url": "/recorridos-literarios/static/media/ionicons.19e65b89.eot"
+  },
+  {
+    "revision": "b351bd62abcd96e924d9f44a3da169a7",
+    "url": "/recorridos-literarios/static/media/Material-Design-Iconic-Font.b351bd62.ttf"
+  },
+  {
+    "revision": "ba2a91dc95e6cfdc4b2a186a7ba83e29",
+    "url": "/recorridos-literarios/static/media/fa-regular-400.ba2a91dc.svg"
+  },
+  {
+    "revision": "ea363ed422723673917901680be9b37c",
+    "url": "/recorridos-literarios/static/media/fa-solid-900.ea363ed4.eot"
+  },
+  {
+    "revision": "132e9759d93e4eefd7cdde0d7a322991",
+    "url": "/recorridos-literarios/static/media/fa-solid-900.132e9759.ttf"
+  },
+  {
+    "revision": "dd4781d1acc57ba4c4808d1b44301201",
+    "url": "/recorridos-literarios/static/media/ionicons.dd4781d1.ttf"
+  },
+  {
+    "revision": "aff28a207631f39ee0272d5cdde43ee7",
+    "url": "/recorridos-literarios/static/media/ionicons.aff28a20.svg"
+  },
+  {
+    "revision": "83e6c29fb363b2f0ea6cc18fefff729c",
+    "url": "/recorridos-literarios/static/media/fa-brands-400.83e6c29f.svg"
+  },
+  {
+    "revision": "de1d242d8acb26ec43c0d071fe78e72d",
+    "url": "/recorridos-literarios/static/media/fa-solid-900.de1d242d.svg"
+  },
+  {
+    "revision": "06ac2d2b3b17da9ded6e",
+    "url": "/recorridos-literarios/static/css/1.4d56ad2d.chunk.css"
+  },
+  {
+    "revision": "052370ae187b9962b3bedc822d3567c3",
+    "url": "/recorridos-literarios/index.html"
+  }
+];
